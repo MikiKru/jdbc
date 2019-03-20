4d4231c9bf09c4497e3f3e8cdafa28d7ceab9062
MikiKru <michal_kruczkowski@o2.pl>
2019-03-20 11:44:59 +0100

    jdbc v3

---
+++ src/controller/RegisterController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class RegisterController {
+
+    @FXML
+    private TextField tf_login;
+
+    @FXML
+    private PasswordField pf_password;
+
+    @FXML
+    private PasswordField pf_password2;
+
+    @FXML
+    private TextField tf_name;
+
+    @FXML
+    private TextField tf_lastname;
+
+    @FXML
+    void clearAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void keyRegisterAction(KeyEvent event) {
+
+    }
+
+    @FXML
+    void registerAction(ActionEvent event) {
+
+    }
+
+}
