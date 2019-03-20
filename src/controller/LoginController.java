af57bcc160391a14956bb3fa068441a43b151cfd
MikiKru <michal_kruczkowski@o2.pl>
2019-03-20 11:24:31 +0100

    jdbc v2

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginController {
@@ -18,18 +20,45 @@ public class LoginController {
     private PasswordField pf_password;
 
     @FXML
-    void loginAction(ActionEvent event) {
-
+    void loginAction(ActionEvent event) throws SQLException {
+        // przygotowuję zapytanie
+        ps = connection.prepareStatement("SELECT * FROM users WHERE email = ? AND password = ?");
+        // przypisauję wartości do ?
+        ps.setString(1, tf_login.getText());
+        ps.setString(2, pf_password.getText());
+        // wykonuje zapytanie
+        // SELECT -> executeQuery()
+        // INSERT, UPDATE, DELETE, CREATE, DROP -> executeUpdate()
+        // wykonałem zapytanie i zwracam wynik do tablicy wielowymiarowej
+        ResultSet resultSet = ps.executeQuery();
+        // przesuwam wskaźnik na pierwszą pozycję i sprawdzam czy jest niepusta
+        if(resultSet.next()){
+            // jeżeli jest niepusta to wyciągam zawartość
+            System.out.println(resultSet.getInt(1));
+            System.out.println(resultSet.getString(2));
+            System.out.println(resultSet.getString(3));
+            System.out.println(resultSet.getString(4));
+            System.out.println(resultSet.getString(5));
+            System.out.println(resultSet.getBoolean(6));
+            System.out.println(resultSet.getString(7));
+            System.out.println(resultSet.getDate(8));
+        } else {
+            System.out.println("Błąd logowania!");
+        }
     }
 
     @FXML
     void registerAction(ActionEvent event) {
 
     }
-
+    // globalne obiekty połączenia do bazy danych
+    DBConnector dbConnector;
+    Connection connection;
+    // globalny obiekt do wykonywania zapytań
+    PreparedStatement ps;
     public void initialize() throws SQLException {
-        DBConnector dbConnector = new DBConnector();
-        Connection connection = dbConnector.initConnection();
+        dbConnector = new DBConnector();
+        connection = dbConnector.initConnection();
     }
 
 }
